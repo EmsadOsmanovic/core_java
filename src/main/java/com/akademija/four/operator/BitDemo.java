077834941b923e16487cc86ceb5cc7bfe872d3b9
osmanoviice <osmemsad@gmail.com>
2025-11-16 12:03:32 +0100

    operatori poređenja i na nivou bita

---
+++ src/main/java/com/akademija/four/operator/BitDemo.java
@@ -0,0 +1,13 @@
+package com.akademija.four.operator;
+
+//operatori na nivou bita - nemojte ih koristiti
+public class BitDemo {
+    public static void main(String[] args) {
+        // 00000....11 -> 0b00100
+        //                0b01000
+        //                0b00010
+        int number = 4;
+        number = number>>2;
+        System.out.println(number);
+    }
+}
