c03d004590b9fc536036f879e4117916376a30e7
osmanoviice <osmemsad@gmail.com>
2025-11-16 11:47:26 +0100

    Prvi source kod Program.java

---
+++ src/main/java/com/akademija/Program.java
@@ -0,0 +1,8 @@
+package com.akademija;
+
+//git gitHub
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Vozdra raja iz Intellij. Sada učimo koristiti git alat");
+    }
+}
